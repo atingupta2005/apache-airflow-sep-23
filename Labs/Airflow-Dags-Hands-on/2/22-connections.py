bae3feb1e57af175c7211c1f945364a2fbca895f
Atin Gupta <atingupta@email.com>
2023-09-26 08:50:33 +0530

    -

---
+++ Labs/Airflow-Dags-Hands-on/2/22-connections.py
@@ -0,0 +1,43 @@
+# pip install 'apache-airflow-providers-http'
+## Open the Airflow homepage and go under Admin - Connections. Click on the plus sign to add a new connection, and specify the http connection parameters:
+## - https://gorest.co.in/public/v2
+
+
+import json
+from datetime import datetime
+from airflow.models import DAG
+from airflow.providers.http.sensors.http import HttpSensor
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.operators.python import PythonOperator
+
+def save_posts(ti) -> None:
+    posts = ti.xcom_pull(task_ids=['get_posts'])
+    with open('/tmp/posts.json', 'w') as f:
+        json.dump(posts[0], f)
+        
+with DAG(
+    dag_id='22-connections',
+    schedule_interval='@daily',
+    start_date=datetime(2022, 3, 1),
+    catchup=False
+) as dag:
+ 
+   task_is_api_active = HttpSensor(
+        task_id='is_api_active',
+        http_conn_id='api_posts',
+        endpoint='posts/'
+    )
+
+   task_get_posts = SimpleHttpOperator(
+        task_id='get_posts',
+        http_conn_id='api_posts',
+        endpoint='posts/',
+        method='GET',
+        response_filter=lambda response: json.loads(response.text),
+        log_response=True
+    )
+   
+   task_save = PythonOperator(
+        task_id='save_posts',
+        python_callable=save_posts
+    )

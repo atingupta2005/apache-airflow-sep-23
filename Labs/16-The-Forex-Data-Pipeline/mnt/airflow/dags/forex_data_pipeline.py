52d6945b75b3b6b6c8414632b05341c700d54476
Atin Gupta <atingupta@email.com>
2023-09-29 08:30:14 +0530

    -

--- Labs/16-The-Forex-Data-Pipeline/mnt/airflow/dags/forex_data_pipeline.py
+++ Labs/16-The-Forex-Data-Pipeline/mnt/airflow/dags/forex_data_pipeline.py
@@ -23,7 +23,7 @@ default_args = {
 }
 
 def download_rates():
-    BASE_URL = "https://gist.githubusercontent.com/atingupta2005/0b0079f62c6a74ef1190b74798a07341/raw/"
+    BASE_URL = "https://gist.githubusercontent.com/marclamberti/f45f872dea4dfd3eaa015a4a1af4b39b/raw/"
     ENDPOINTS = {
         'USD': 'api_forex_exchange_usd.json',
         'EUR': 'api_forex_exchange_eur.json'
@@ -44,13 +44,13 @@ def download_rates():
 def _get_message() -> str:
     return "Hi from forex_data_pipeline"
 
-with DAG("forex_data_pipeline", start_date=datetime(2021, 1 ,1),
+with DAG("forex_data_pipeline", start_date=datetime(2021, 1 ,1), 
     schedule_interval="@daily", default_args=default_args, catchup=False) as dag:
 
     is_forex_rates_available = HttpSensor(
         task_id="is_forex_rates_available",
         http_conn_id="forex_api",
-        endpoint="atingupta2005/0b0079f62c6a74ef1190b74798a07341",
+        endpoint="marclamberti/f45f872dea4dfd3eaa015a4a1af4b39b",
         response_check=lambda response: "rates" in response.text,
         poke_interval=5,
         timeout=20
@@ -117,7 +117,7 @@ with DAG("forex_data_pipeline", start_date=datetime(2021, 1 ,1),
         message=_get_message(),
         channel="#monitoring"
     )
-
-    is_forex_rates_available >> is_forex_currencies_file_available >> downloading_rates >> saving_rates
+    
+    is_forex_rates_available >> is_forex_currencies_file_available >> downloading_rates >> saving_rates 
     saving_rates >> creating_forex_rates_table >> forex_processing
-    forex_processing >> send_email_notification >> send_slack_notification
+    forex_processing >> send_email_notification >> send_slack_notification 
